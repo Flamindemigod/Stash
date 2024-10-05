9f20e9320783949c3436d8c806dd8b57f438f3b7
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2024-10-05 18:47:15 +0100

    added some useage stuff and a version tracking during build step

--- nob.c
+++ nob.c
@@ -1,16 +1,42 @@
-//#include <assert.h>
+// #include <assert.h>
 
+#include <stdio.h>
 #define NOB_IMPLEMENTATION
 #include "./nob.h"
 
+int getCommitHash(Nob_Cmd *cmd) {
+  int retVal = 1;
+  Nob_String_Builder sb = {0};
+  nob_cmd_append(cmd, "/bin/sh", "-c",
+                 "echo $(git rev-parse HEAD) > ./build/commit");
+  retVal = nob_cmd_run_sync(*cmd);
+  cmd->count = 0;
+  if (!retVal)
+    return retVal;
+  retVal = nob_read_entire_file("./build/commit", &sb);
+  Nob_String_View sv = nob_sv_from_parts(sb.items, sb.count);
+  sv = nob_sv_trim(sv);
+  FILE *fd = fopen("./build/config.h", "wb");
+  if (fd == NULL) {
+    nob_log(NOB_ERROR, "Failed to open build/config.h");
+  }
+  fputs(nob_temp_sprintf("#define VERSION \"%s\"\n", nob_temp_sv_to_cstr(sv)), fd);
+  fclose(fd);
+  nob_sb_free(sb);
+  return retVal;
+}
+
 int build_stash(Nob_Cmd *cmd) {
   int retVal = 1;
-  if (nob_needs_rebuild1("./build/stash", "./main.c")){
-  nob_cmd_append(cmd, "cc");
-  nob_cmd_append(cmd, "-Wall", "-Wextra", "-ggdb");
-  nob_cmd_append(cmd, "-o", "./build/stash");
-  nob_cmd_append(cmd, "main.c");
-  retVal = nob_cmd_run_sync(*cmd);
+  if (nob_needs_rebuild1("./build/stash", "./main.c")) {
+    if (!getCommitHash(cmd)) {
+      return 0;
+    };
+    nob_cmd_append(cmd, "cc");
+    nob_cmd_append(cmd, "-Wall", "-Wextra", "-ggdb");
+    nob_cmd_append(cmd, "-o", "./build/stash");
+    nob_cmd_append(cmd, "main.c");
+    retVal = nob_cmd_run_sync(*cmd);
   }
   cmd->count = 0;
   return retVal;
@@ -37,16 +63,16 @@ int main(int argc, char *argv[]) {
     nob_return_defer(EXIT_FAILURE);
   }
 
-  if (argc <= 0){
+  if (argc <= 0) {
     nob_log(NOB_INFO, "Exiting");
     nob_return_defer(EXIT_SUCCESS);
   }
   const char *subcommand = nob_shift_args(&argc, &argv);
   nob_log(NOB_INFO, "%s", subcommand);
-  if (strcmp(subcommand, "run") == 0){
+  if (strcmp(subcommand, "run") == 0) {
     run_stash(&cmd);
   }
-  defer:
-    nob_cmd_free(cmd);
-    return result;
+defer:
+  nob_cmd_free(cmd);
+  return result;
 }
